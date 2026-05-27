310052883da6b5e7d43cc2900ea0057e6fe4628f
Anton Parfiankou <a.l.parfiakou@gmail.com>
2026-05-27 09:30:32 +0300

    123123

--- src/treatments/dto/update-treatment.dto.ts
+++ src/treatments/dto/update-treatment.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTreatmentDto } from './create-treatment.dto';
+import { IsDate, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateTreatmentDto extends PartialType(CreateTreatmentDto) {}
+export class UpdateTreatmentDto extends PartialType(CreateTreatmentDto) {
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  actualDate?: Date;
+}
